eeeb55a2eb6a2d0f351d53dcf0f696ccb55c67cd
sonesuke <iamsonesuke@gmail.com>
2024-10-12 16:25:15 +0900

    setup k6

--- django/app/sample/views.py
+++ django/app/sample/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+import time
 
 # Create your views here.
 def index(request):
+    time.sleep(1)
     return HttpResponse("Hello, world. You're at the sample index.")
