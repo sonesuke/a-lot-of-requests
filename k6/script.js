d7f4c382b3322b487b5acd760f140697f5141044
sonesuke <iamsonesuke@gmail.com>
2024-10-12 22:13:45 +0900

    setup uvicorn

--- k6/script.js
+++ k6/script.js
@@ -1,9 +1,9 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 1000,
+  vus: 10,
   duration: '10s',
 };
 export default function () {
-  http.get('http://web:8080/django/workload/no-load');
+  http.get('http://web:8080/django/workload/prime-number');
 }
